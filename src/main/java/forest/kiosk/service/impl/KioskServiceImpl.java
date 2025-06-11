173c1d9f9bc0f49a0b01432ca7dac0d3a4e70876
JungNamYoung <kmiss75@daum.net>
2025-06-11 18:28:37 +0900

    수정3

--- src/main/java/forest/kiosk/service/impl/KioskServiceImpl.java
+++ src/main/java/forest/kiosk/service/impl/KioskServiceImpl.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import forest.kiosk.service.KioskService;
 import forest.kiosk.vo.KioskCol;
--- src/main/webapp/WEB-INF/views/forest/kioskContent.jsp
+++ src/main/webapp/WEB-INF/views/forest/kioskContent.jsp
@@ -1,3 +1,7 @@
+<%@ page contentType="text/html;charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+
 <div class="content-header">
   <div class="container-fluid">
     <div class="row mb-2">
